c984a83a9b34f0d3c6526a0473bf546093c04002
lelz99 <emanuelesimonetti99@gmail.com>
2024-04-23 12:38:26 +0200

    fetched apartments

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
-<script setup>
+<script>
+import axios from 'axios';
+const baseUri = 'http://localhost:8000/api/';
+
 export default {
-  name: 'Boolbnb'
+  name: 'Boolbnb',
+  data: () => ({ apartments: [] }),
+  methods: {
+
+    fetchApartments() {
+      axios.get(baseUri + 'apartments/').then(res => { this.apartments = res.data })
+    }
+
+  },
+  created() {
+    this.fetchApartments()
+  }
 }
 </script>
 
 <template>
-
+  <ul>
+    <li v-for="apartment in apartments" :key="apartment.id">{{ apartment.title }}</li>
+  </ul>
 </template>
 
 <style scoped></style>
