ea3e3e171565c2aa2d20c2d6f36019a0421c6a44
lelz99 <emanuelesimonetti99@gmail.com>
2024-04-23 12:21:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+export default {
+  name: 'Boolbnb'
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
