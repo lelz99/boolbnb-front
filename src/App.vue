a535525678897a043ba46ac87706105065e260bd
AlessiaOsman <alessia.osman@gmail.com>
2024-04-24 18:32:40 +0200

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
 const baseUri = 'http://localhost:8000/api/';
 
 export default {
   name: 'Boolbnb',
+  components: {AppHeader},
   data: () => ({ apartments: [] }),
   methods: {
 
@@ -19,9 +21,15 @@ export default {
 </script>
 
 <template>
-  <ul>
+  <AppHeader/>
+  <main class="container mt-4">
+    <ul>
     <li v-for="apartment in apartments" :key="apartment.id">{{ apartment.title }}</li>
   </ul>
+  </main>
+  
 </template>
 
-<style scoped></style>
+<style lang="scss">
+  @use'./assets/scss/style.scss'
+</style>
