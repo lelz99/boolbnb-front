4948c1d069fb067ce65885f0220a9d17da6f4fbb
AlessiaOsman <alessia.osman@gmail.com>
2024-04-26 12:07:46 +0200

    added Apartment Card

---
+++ src/components/apartments/ApartmentCard.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'ApartmentCard',
+    props: { apartment: Object }
+}
+</script>
+<template>
+    <div class="card">
+        <img :src="apartment.cover_image" :alt="apartment.title">
+        <h2>{{ apartment.title }}</h2>
+    </div>
+</template>
