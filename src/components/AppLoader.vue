358d781643c02a3fd7f347c50e69861983a2ef53
AlessiaOsman <alessia.osman@gmail.com>
2024-04-26 12:08:45 +0200

    added app loader

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'AppLoader',
+}
+</script>
+
+<template>
+    <div class="loader-overlay">
+        <div class="spinner-border text-primary" role="status">
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.loader-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(.5, .5, .5, .5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
+import AppLoader from './components/AppLoader.vue';
 
-createApp(App).mount('#app')
+createApp(App)
+.component('Apploader', AppLoader)
+.mount('#app')
