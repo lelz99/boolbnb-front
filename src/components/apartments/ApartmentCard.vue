6875a2cec41ae093d846327f6a7fd3831ad613b9
AlessiaOsman <alessia.osman@gmail.com>
2024-04-29 18:25:43 +0200

    card css

--- src/components/apartments/ApartmentCard.vue
+++ src/components/apartments/ApartmentCard.vue
@@ -7,7 +7,10 @@ export default {
 <template>
     <div class="col-sm-12 col-lg-6 col-xl-3 col-xl-2 my-4">
         <div class="card card-apartment p-3">
+            <figure>
                 <img class="card-image card-img-top img-fluid rounded" :src="apartment.cover_image" :alt="apartment.title">
+            </figure>
+                
                 <div class="card-body">
             <h5 class="mt-3">{{ apartment.title }}</h5>
             <p class="mb-2">{{ apartment.address }}</p>
@@ -23,10 +26,17 @@ export default {
 <style lang="scss" scoped>
     .card-apartment {
     
-        .card-image{
-            max-height: 250px;
+        figure{
+            width: 100%;
+            height: 300px;
+            overflow: hidden;
+
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
         }
-        
 
     .card-services {
         display: flex;
