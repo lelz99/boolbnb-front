82a58c1b1ac126f0ebb5ae57a51f31e9b42e5e55
AlessiaOsman <alessia.osman@gmail.com>
2024-04-29 18:13:44 +0200

    css card

--- src/components/apartments/ApartmentCard.vue
+++ src/components/apartments/ApartmentCard.vue
@@ -5,30 +5,34 @@ export default {
 }
 </script>
 <template>
-   
-    <div class="card-apartment col-sm-12 col-lg-6 col-xl-3 col-xl-2 my-4">
-        <img class="card-image img-fluid rounded" :src="apartment.cover_image" :alt="apartment.title">
-        <h5 class="mt-3">{{ apartment.title }}</h5>
-        <p class="mb-2">{{ apartment.address }}</p>
-        <ul class="card-services">
-            <li v-for="service in apartment.services"><i :class="service.icon"></i></li>
-        </ul>
+    <div class="col-sm-12 col-lg-6 col-xl-3 col-xl-2 my-4">
+        <div class="card card-apartment p-3">
+                <img class="card-image card-img-top img-fluid rounded" :src="apartment.cover_image" :alt="apartment.title">
+                <div class="card-body">
+            <h5 class="mt-3">{{ apartment.title }}</h5>
+            <p class="mb-2">{{ apartment.address }}</p>
+            <ul class="card-services">
+                <li v-for="service in apartment.services"><i :class="service.icon"></i></li>
+            </ul>
+        </div>
+        </div>
+
     </div>
 </template>
 
 <style lang="scss" scoped>
-
-.card-apartment{
+    .card-apartment {
     
+        .card-image{
+            max-height: 250px;
+        }
+        
 
     .card-services {
-    display: flex;
-    gap: 16px;
-    overflow-x: auto; 
-    white-space: nowrap;
+        display: flex;
+        gap: 16px;
+        overflow-x: auto;
+        white-space: nowrap;
+    }
 }
-}
-
-
-
 </style>
