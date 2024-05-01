5c4b75a2e04cc25e42c7b66d8100a8ca9e7c431c
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-05-02 01:32:37 +0200

    other fixes to show

---
+++ src/components/apartments/ShowCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'ShowCard',
+    props: { apartment: Object, isDetail: Boolean },
+    computed: {
+        // abstract() {
+        //     const abstract = this.project.content.slice(0, 200);
+        //     return abstract + '[...]';
+        // },
+        // publicationDate() {
+        //     const date = new Date(this.project.created_at);
+        //     let day = date.getDate();
+        //     let month = date.getMonth();
+        //     const year = date.getFullYear();
+        //     let hours = date.getHours();
+        //     let minutes = date.getMinutes();
+        //     // const seconds = date.getSeconds();
+
+        //     if (day < 10) day = '0' + day;
+        //     if (month < 10) month = '0' + month;
+        //     if (hours < 10) hours = '0' + hours;
+        //     if (minutes < 10) minutes = '0' + minutes;
+
+        //     return `${day}/${month}/${year} alle ${hours}:${minutes}`;
+        // }
+    }
+};
+</script>
--- src/pages/ShowPage.vue
+++ src/pages/ShowPage.vue
@@ -3,10 +3,11 @@ import axios from 'axios';
 import { store } from '../data/store';
 import AppLoader from '../components/AppLoader.vue';
 import AppAlert from '../components/AppAlert.vue';
+import ShowCard from '../components/apartments/ShowCard.vue';
 
 export default {
     name: 'ShowPage',
-    components: { AppLoader, AppAlert },
+    components: { AppLoader, AppAlert, ShowCard },
     data: () => ({
         store,
         apartment: null
@@ -15,16 +16,27 @@ export default {
         test(test) {
             console.log(test);
         },
+        getApartment() {
+            store.isLoading = true;
+            axios.get(store.baseUri + this.$route.params.slug)
+                .then(res => { this.apartment = res.data })
+                .catch(err => { console.error(err) })
+                .then(() => { store.isLoading = false });
+        }
     },
     computed: {
         // No computed atm
     },
     created() {
         this.test("test");
+        this.getApartment();
     }
 }
 </script>
 
-<template></template>
+<template>
+    <h1 class="py-3 text-center">SHOW APPARTAMENTO</h1>
+    <ShowCard v-if="!store.isLoading && apartment" :apartment="apartment" :isDetail="true" />
+</template>
 
 <style></style>
