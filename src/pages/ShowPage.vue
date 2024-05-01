64761f22a791251ea144b00ca4cbc56f4f097851
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-05-01 18:06:47 +0200

    created show page

---
+++ src/pages/ShowPage.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import { store } from '../data/store';
+import AppLoader from '../components/AppLoader.vue';
+import AppAlert from '../components/AppAlert.vue';
+
+export default {
+    name: 'FilteredPage',
+    components: { ApartmentCard, ApartmentList, AppAlert },
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        test(test) {
+            console.log(test);
+        },
+    },
+    computed: {
+        // No computed atm
+    },
+    created() {
+        this.test("test");
+    }
+}
+</script>
+
+<template></template>
+
+<style></style>
