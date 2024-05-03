137ae3ac47b96bed445f5b9c9e0e8a444919a751
FrancescoM95 <mon95fran@gmail.com>
2024-05-03 11:13:39 +0200

    filteredpage style

--- src/pages/FilteredPage.vue
+++ src/pages/FilteredPage.vue
@@ -175,7 +175,7 @@ export default {
         <div class="col-9">
             <div class="mt-3 ms-3">
                 <div class="row form-group">
-                    <div v-for="service in services" :key="service.id" class="form-check form-check-inline col-3">
+                    <div v-for="service in services" :key="service.id" class="form-check form-check-inline col-3 text-light">
                         <label class="check-container d-flex align-items-center">
                             <input class="form-check-input" type="checkbox" :id="service.id" v-model="filterService"
                                 :value="service.id">
@@ -195,13 +195,13 @@ export default {
 
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="beds" class="form-label">Numero Letti</label>
+                        <label for="beds" class="form-label text-light">Numero Letti</label>
                         <input type="number" min="1" class="form-control" id="beds" v-model="store.beds">
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="rooms" class="form-label">Numero Stanze</label>
+                        <label for="rooms" class="form-label text-light">Numero Stanze</label>
                         <input type="number" class="form-control" id="rooms" v-model="store.rooms" min="1">
                     </div>
                 </div>
